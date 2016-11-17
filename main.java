058c3d975e6a8878d4310ec2bcd4b18d15b2c9e5
AllXS29 <alexis.hellouin@gmail.com>
2016-11-17 10:49:22 +0100

    code improvement
    - Modification on algorithm, if Skynet agent is not linked to an exit
    gateway, sever one link who is connected to the Skynet agent.
    - List are changed to array :
    * 1 dimension int array for exits, with their node number
    * 2 dimension boolean array for links, set to false by default except
    when the link exists set to true
    Note : links are added in both way (e.g. link between 12 and 25 will
    exist in 12->25 and 25->12)
    Results : Test 4 is finished with more than 50 links (52 links
    remaining)

--- main.java
+++ main.java
@@ -13,16 +13,18 @@ class Player {
         int N = in.nextInt(); // the total number of nodes in the level, including the gateways
         int L = in.nextInt(); // the number of links
         int E = in.nextInt(); // the number of exit gateways
-		List <Integer> egList = new ArrayList(); // future list of existing exit gateway
-		List <String> linkList = new ArrayList(); // future list of all node links
+		int[] exits = new  int[E]; // future array of existing exit gateway
+		boolean[][] links = new boolean[N][N]; // future list of all node links
         for (int i = 0; i < L; i++) {
             int N1 = in.nextInt(); // N1 and N2 defines a link between these nodes
             int N2 = in.nextInt();
-			 linkList.add(N1 + " " + N2);// populate the link list
-        }
+			//populate the array, if the link exist it is set to true
+			links[N1][N2] = true;
+			links[N2][N1] = true;
+		}
         for (int i = 0; i < E; i++) {
             int EI = in.nextInt(); // the index of a gateway node
-            egList.add(EI); // populate the list of all the gateway exit node
+            exits[i] = EI; // populate the array of all the gateway exit node
         }
 
         // game loop
@@ -30,26 +32,30 @@ class Player {
             int SI = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
             //test if the Skynet agent is near an exit gateway
 			boolean severed = false; // boolean if a link has been severed in the loop
-			for(Integer exit : exitList){
-                if(linkList.contains(SI + " " + exit)){
+			for(Integer exit : exits){
+                if(links[SI][exit]){
 					//Sever the link and remove it from the list
-					severed = true; // link being severed
-                    System.out.println(SI + " " + exit);
-                    linkList.remove(SI + " " + exit);
-                    break;
-                }else if (linkList.contains(exit + " " + SI)){
-					//Sever the link and remove it from the list
-                    severed = true; // link being severed
-					System.out.println(exit + " " + SI);
-                    linkList.remove(exit + " " + SI);
+					severed = true; // link is being severed
+					//the link does not exist anymore
+					links[SI][exit] = false;
+					links[exit][SI] = false;
+					System.out.println(SI + " " + exit);
+					//quit the loop
                     break;
                 }
             }
-			//no link were severed cut the first link from the list
-			//TODO : create algorithm choosing which link to sever for better effeciency
+			//no link were severed cut one of the links near the Skynet agent 
 			if(!severed){
-				System.out.println(linkList.get(0));
-				linkList.remove(0);
+				for(int i = 0; i < links.length; i++){
+					if(links[SI][i]){
+						//the link does not exist anymore
+						links[SI][i] = false;
+						links[i][SI] = false;
+						System.out.println(i + " " + SI);
+						//quit the loop
+						break;
+					}
+				}
 			}
         }
     }
